ec5dbe7926012b9934d5b796d4ab5b05a5c8d4d0
wyl <2605214840@qq.com>
2019-07-01 10:16:19 +0800

    企业微信认证

---
+++ diboot-shiro-wx-cp/src/main/java/com/diboot/shiro/wx/cp/controller/CpAuthTokenController.java
@@ -0,0 +1,121 @@
+package com.diboot.shiro.wx.cp.controller;
+
+import com.diboot.core.util.V;
+import com.diboot.core.vo.JsonResult;
+import com.diboot.core.vo.Status;
+import com.diboot.shiro.config.AuthType;
+import com.diboot.shiro.jwt.BaseJwtAuthenticationToken;
+import com.diboot.shiro.service.AuthWayService;
+import com.diboot.shiro.util.JwtHelper;
+import com.diboot.shiro.wx.cp.config.WxCpConfig;
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.cp.api.WxCpService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cpToken")
+public class CpAuthTokenController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CpAuthTokenController.class);
+
+    @Autowired
+    private Map<String, AuthWayService> authWayServiceMap;
+
+    @Autowired
+    private WxCpService wxCpService;
+
+    /**
+     * 企业微信创建OAuth认证链接(为了获取授权所需code)
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/buildOAuthUrl")
+    public JsonResult buildOAuthUrl4cp(HttpServletRequest request) throws Exception{
+        String url = request.getParameter("url");
+        String agentId = request.getParameter("agentId");//应用ID
+        if (V.isEmpty(url)){
+            return new JsonResult(Status.FAIL_OPERATION, new String[]{"url为空，获取OAuth链接失败"});
+        }
+        wxCpService = WxCpConfig.getCpService(Integer.parseInt(agentId));
+
+        String oauthUrl = wxCpService.getOauth2Service().buildAuthorizationUrl(url, null, WxConsts.OAuth2Scope.SNSAPI_PRIVATEINFO);
+        return new JsonResult(Status.OK, oauthUrl, new String[]{"获取OAuth链接成功"});
+    }
+
+    /**
+     * 企业微信的回调授权登录认证
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/apply")
+    public JsonResult applyTokenByOAuth2cp(HttpServletRequest request) throws Exception{
+        String code = request.getParameter("code");
+        String userId = "";
+        if (JwtHelper.isRequestTokenEffective(request)){
+            String account = JwtHelper.getAccountFromToken(JwtHelper.getRequestToken(request));
+            if (account == null){
+                // 如果有code并且token已过期，则使用code获取userId
+                if (V.isEmpty(code)){
+                    return new JsonResult(Status.FAIL_INVALID_TOKEN, new String[]{"token已过期"});
+                }
+            } else {
+                userId = account;
+            }
+        }
+
+        // 如果userId没有通过token获取到，则通过code获取
+        if (V.isEmpty(userId)){
+            if (V.isEmpty(code)){
+                // 如果没有code参数，则返回提示信息
+                return new JsonResult(Status.FAIL_INVALID_TOKEN, new String[]{"请重新进入页面"});
+            }
+            String[] res = wxCpService.getOauth2Service().getUserInfo(code);
+            userId = res[0];
+        }
+
+        // 如果没有获取到userId，则返回提示信息
+        if (V.isEmpty(userId)){
+            return new JsonResult(Status.FAIL_INVALID_TOKEN, new String[]{"获取信息失败"});
+        }
+
+        // 设置token
+        BaseJwtAuthenticationToken authToken = new BaseJwtAuthenticationToken(authWayServiceMap, userId, AuthType.WX_CP);
+        // 获取当前的Subject
+        Subject subject = SecurityUtils.getSubject();
+        String token = null;
+        String errorMsg = null;
+
+        try {
+            subject.login(authToken);
+            //验证是否登录成功
+            if(subject.isAuthenticated()){
+                token = (String)authToken.getCredentials();
+                logger.debug("userId[" + userId + "]申请token成功！authtoken="+token);
+            }
+        }
+        catch(Exception e){
+            logger.error("登录失败", e);
+        }
+
+        if (V.isEmpty(token)){
+            String msg = V.notEmpty(errorMsg) ? errorMsg : "申请token失败";
+            return new JsonResult(Status.FAIL_INVALID_TOKEN, new String[]{msg});
+        }
+
+        return new JsonResult(Status.OK, token, new String[]{"申请token成功"});
+    }
+
+}
---
+++ diboot-shiro-wx-cp/src/main/java/com/diboot/shiro/wx/cp/service/impl/WxCpMemberServiceImpl.java
@@ -0,0 +1,19 @@
+package com.diboot.shiro.wx.cp.service.impl;
+
+import com.diboot.core.service.impl.BaseServiceImpl;
+import com.diboot.shiro.wx.cp.entity.WxCpMember;
+import com.diboot.shiro.wx.cp.mapper.WxCpMemberMapper;
+import com.diboot.shiro.wx.cp.service.WxCpMemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/***
+ * @author Wangyl
+ * @version v2.0
+ * @date 2019/6/10
+ */
+@Service
+@Slf4j
+public class WxCpMemberServiceImpl extends BaseServiceImpl<WxCpMemberMapper, WxCpMember> implements WxCpMemberService {
+
+}
