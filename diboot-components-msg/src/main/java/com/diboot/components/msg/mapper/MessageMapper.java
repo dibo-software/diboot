48476ae6d6167818a6c437499eaf9d086c8afc9d
wyldusername <2605214840@qq.com>
2019-07-09 17:10:02 +0800

    移植msg组件

---
+++ diboot-components-msg/src/main/java/com/diboot/components/msg/mapper/MessageMapper.java
@@ -0,0 +1,7 @@
+package com.diboot.components.msg.mapper;
+
+import com.diboot.components.msg.entity.Message;
+import com.diboot.core.mapper.BaseCrudMapper;
+
+public interface MessageMapper extends BaseCrudMapper<Message> {
+}
---
+++ diboot-components-msg/src/main/java/com/diboot/components/msg/mapper/MessageTemplateMapper.java
@@ -0,0 +1,7 @@
+package com.diboot.components.msg.mapper;
+
+import com.diboot.components.msg.entity.MessageTemplate;
+import com.diboot.core.mapper.BaseCrudMapper;
+
+public interface MessageTemplateMapper extends BaseCrudMapper<MessageTemplate> {
+}
