9b962e91d9417ab81e379d16ef49d132b17f6cd1
mazhicheng <new123456>
2019-10-23 21:16:45 +0800

    + fix: 绑定注解的condition中有等于字符串附加条件时执行异常的问题

---
+++ diboot-core/src/test/java/diboot/core/test/binder/vo/UserVO.java
@@ -0,0 +1,29 @@
+package diboot.core.test.binder.vo;
+
+import com.diboot.core.binding.annotation.BindDict;
+import com.diboot.core.binding.annotation.BindField;
+import diboot.core.test.binder.entity.Department;
+import diboot.core.test.binder.entity.Organization;
+import diboot.core.test.binder.entity.User;
+
+/**
+ * <Description>
+ *
+ * @author Mazhicheng
+ * @version v2.0
+ * @date 2019/06/22
+ */
+public class UserVO extends User{
+    private static final long serialVersionUID = 3526115343377985709L;
+
+    // 字段关联，附加更多条件
+    @BindField(entity= Department.class, field="name", condition="this.department_id=id AND parent_id IS NOT NULL AND name = '研发组'")
+    private String deptName;
+
+    public String getDeptName() {
+        return deptName;
+    }
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
