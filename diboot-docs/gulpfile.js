5e55b5c12063bb1e0469647723fbfac6dc4bdec8
Zhaoyang <yangzhao635226@163.com>
2019-11-26 14:25:55 +0800

    增加编译文档脚本

--- diboot-docs/gulpfile.js
+++ diboot-docs/gulpfile.js
@@ -10,6 +10,7 @@ gulp.task('default', function () {
 #cnzz_stat_icon_1278228116{
     display: block;
     margin: 0 auto;
+    text-align: center;
 }
 </style>
 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278228116'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278228116%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>`
