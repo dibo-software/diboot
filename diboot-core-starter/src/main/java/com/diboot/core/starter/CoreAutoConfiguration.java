5f93f71c3d53cbb69b829ade0ada58dcd67a7d39
wuy <1311695042@qq.com>
2020-12-04 21:42:53 +0800

    fix: 修复升级mp后分页配置失效

--- diboot-core-starter/src/main/java/com/diboot/core/starter/CoreAutoConfiguration.java
+++ diboot-core-starter/src/main/java/com/diboot/core/starter/CoreAutoConfiguration.java
@@ -15,8 +15,6 @@
  */
 package com.diboot.core.starter;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.diboot.core.util.D;
