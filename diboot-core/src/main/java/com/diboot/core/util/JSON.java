7badba328f7474d4f92196a9d0ce1a05f5310387
wuy <1311695042@qq.com>
2020-12-17 16:34:27 +0800

    add: 增加JSON转化复杂类型的对象方法

--- diboot-core/src/main/java/com/diboot/core/util/JSON.java
+++ diboot-core/src/main/java/com/diboot/core/util/JSON.java
@@ -15,18 +15,14 @@
  */
 package com.diboot.core.util;
 
-import com.diboot.core.entity.BaseEntity;
-import com.diboot.core.entity.Dictionary;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,121 +34,135 @@ import java.util.Map;
  * @date 2019/01/01
  */
 public class JSON {
-	private static final Logger log = LoggerFactory.getLogger(JSON.class);
+    private static final Logger log = LoggerFactory.getLogger(JSON.class);
 
-	private static ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper = new ObjectMapper();
 
-	static {
-		// 时间转化配置：解决InvalidFormatException: Can not deserialize value of type java.util.Date
-		mapper.setDateFormat(new SimpleDateFormat(D.FORMAT_DATETIME_Y4MDHMS));
-		// 如果不存在的属性，不转化，否则报错：com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    static {
+        // 时间转化配置：解决InvalidFormatException: Can not deserialize value of type java.util.Date
+        mapper.setDateFormat(new SimpleDateFormat(D.FORMAT_DATETIME_Y4MDHMS));
+        // 如果不存在的属性，不转化，否则报错：com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-	}
+    }
 
-	/**
-	 * 将Java对象转换为Json String
-	 * @param object
-	 * @return
-	 */
-	public static String stringify(Object object){
-		return toJSONString(object);
-	}
+    /**
+     * 将Java对象转换为Json String
+     *
+     * @param object
+     * @return
+     */
+    public static String stringify(Object object) {
+        return toJSONString(object);
+    }
 
-	/**
-	 * 转换对象为JSON字符串
-	 * @param model
-	 * @return
-	 */
-	public static String toJSONString(Object model){
-		try{
-			String json = mapper.writeValueAsString(model);
-			return json;
-		}
-		catch (Exception e){
-			log.error("Java转Json异常", e);
-			return null;
-		}
-	}
+    /**
+     * 转换对象为JSON字符串
+     *
+     * @param model
+     * @return
+     */
+    public static String toJSONString(Object model) {
+        try {
+            String json = mapper.writeValueAsString(model);
+            return json;
+        } catch (Exception e) {
+            log.error("Java转Json异常", e);
+            return null;
+        }
+    }
 
-	/***
-	 * 将JSON字符串转换为java对象
-	 * @param jsonStr
-	 * @param clazz
-	 * @return
-	 */
-	public static <T> T toJavaObject(String jsonStr, Class<T> clazz){
-		try{
-			T model = mapper.readValue(jsonStr, clazz);
-			return model;
-		}
-		catch (Exception e){
-			log.error("Json转Java异常", e);
-			return null;
-		}
-	}
+    /***
+     * 将JSON字符串转换为java对象
+     * @param jsonStr
+     * @param clazz
+     * @return
+     */
+    public static <T> T toJavaObject(String jsonStr, Class<T> clazz) {
+        try {
+            T model = mapper.readValue(jsonStr, clazz);
+            return model;
+        } catch (Exception e) {
+            log.error("Json转Java异常", e);
+            return null;
+        }
+    }
 
-	/***
-	 * 将JSON字符串转换为Map<String, Object></>对象
-	 * @param jsonStr
-	 * @return
-	 */
-	public static Map<String, Object> parseObject(String jsonStr){
-		try{
-			JavaType javaType = mapper.getTypeFactory().constructParametricType(Map.class, String.class, Object.class);
-			return mapper.readValue(jsonStr, javaType);
-		}
-		catch (Exception e){
-			log.error("Json转Java异常", e);
-			return null;
-		}
-	}
+    /***
+     * 将JSON字符串转换为Map<String, Object></>对象
+     * @param jsonStr
+     * @return
+     */
+    public static Map<String, Object> parseObject(String jsonStr) {
+        try {
+            JavaType javaType = mapper.getTypeFactory().constructParametricType(Map.class, String.class, Object.class);
+            return mapper.readValue(jsonStr, javaType);
+        } catch (Exception e) {
+            log.error("Json转Java异常", e);
+            return null;
+        }
+    }
 
-	/***
-	 * 将JSON字符串转换为java对象
-	 * @param jsonStr
-	 * @param clazz
-	 * @return
-	 */
-	public static <T> T parseObject(String jsonStr, Class<T> clazz){
-		return toJavaObject(jsonStr, clazz);
-	}
+    /***
+     * 将JSON字符串转换为java对象
+     * @param jsonStr
+     * @param clazz
+     * @return
+     */
+    public static <T> T parseObject(String jsonStr, Class<T> clazz) {
+        return toJavaObject(jsonStr, clazz);
+    }
+
+    /***
+     * 将JSON字符串转换为复杂类型的Java对象
+     * @param jsonStr
+     * @param typeReference
+     * @return
+     */
+    public static <T> T parseObject(String jsonStr, TypeReference<T> typeReference) {
+        try {
+            T model = mapper.readValue(jsonStr, typeReference);
+            return model;
+        } catch (Exception e) {
+            log.error("Json转Java异常", e);
+            return null;
+        }
+    }
 
 
-	/***
-	 * 将JSON字符串转换为list对象
-	 * @param jsonStr
-	 * @return
-	 */
-	public static <T> List<T> parseArray(String jsonStr, Class<T> clazz){
-		try{
-			JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, clazz);
-			return mapper.readValue(jsonStr, javaType);
-		}
-		catch (Exception e){
-			log.error("Json转Java异常", e);
-			return null;
-		}
-	}
+    /***
+     * 将JSON字符串转换为list对象
+     * @param jsonStr
+     * @return
+     */
+    public static <T> List<T> parseArray(String jsonStr, Class<T> clazz) {
+        try {
+            JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, clazz);
+            return mapper.readValue(jsonStr, javaType);
+        } catch (Exception e) {
+            log.error("Json转Java异常", e);
+            return null;
+        }
+    }
 
-	/***
-	 * 将JSON字符串转换为java对象
-	 * @param jsonStr
-	 * @return
-	 */
-	public static Map toMap(String jsonStr){
-		return toJavaObject(jsonStr, Map.class);
-	}
+    /***
+     * 将JSON字符串转换为java对象
+     * @param jsonStr
+     * @return
+     */
+    public static Map toMap(String jsonStr) {
+        return toJavaObject(jsonStr, Map.class);
+    }
 
-	/***
-	 * 将JSON字符串转换为Map对象
-	 * @param jsonStr
-	 * @return
-	 */
-	public static LinkedHashMap toLinkedHashMap(String jsonStr){
-		if(V.isEmpty(jsonStr)){
-			return null;
-		}
-		return toJavaObject(jsonStr, LinkedHashMap.class);
-	}
+    /***
+     * 将JSON字符串转换为Map对象
+     * @param jsonStr
+     * @return
+     */
+    public static LinkedHashMap toLinkedHashMap(String jsonStr) {
+        if (V.isEmpty(jsonStr)) {
+            return null;
+        }
+        return toJavaObject(jsonStr, LinkedHashMap.class);
+    }
 }
