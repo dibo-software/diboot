2e2865166ae4e37724bfe86b548f099d457cfeec
wuy <1311695042@qq.com>
2021-01-12 13:40:14 +0800

    add: 增加license和版权信息

--- diboot-scheduler-starter/src/main/java/com/diboot/scheduler/vo/ScheduleJobLogVO.java
+++ diboot-scheduler-starter/src/main/java/com/diboot/scheduler/vo/ScheduleJobLogVO.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright (c) 2015-2021, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.diboot.scheduler.vo;
 
-import com.diboot.core.binding.annotation.BindDict;
 import com.diboot.core.binding.annotation.BindField;
 import com.diboot.core.config.Cons;
 import com.diboot.iam.entity.IamUser;
@@ -11,6 +25,7 @@ import lombok.experimental.Accessors;
 
 /**
  * 定时任务日志VO定义
+ *
  * @author JerryMa
  * @version 2.2.0
  * @date 2020-11-26
@@ -21,10 +36,11 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class ScheduleJobLogVO extends ScheduleJobLog {
     private static final long serialVersionUID = 4753135850894402717L;
+
     /**
      * 状态
      */
-    public String getRunStatusLabel(){
+    public String getRunStatusLabel() {
         return Cons.RESULT_STATUS.getLabel(this.getRunStatus());
     }
 
