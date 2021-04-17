21fdeccdf7f98fa15cc0107afb0325735a4b92fd
JerryMa <mazc@dibo.ltd>
2021-04-17 10:52:40 +0800

    + 基于spring CacheManager的内存缓存扩展

---
+++ diboot-core/src/main/java/com/diboot/core/cache/StaticMemoryCacheManager.java
@@ -0,0 +1,12 @@
+package com.diboot.core.cache;
+
+/**
+ * 静态不变化的数据内存缓存manager
+ * @author JerryMa
+ * @version v2.2.1
+ * @date 2021/4/17
+ * Copyright © diboot.com
+ */
+public class StaticMemoryCacheManager extends BaseCacheManager{
+
+}
