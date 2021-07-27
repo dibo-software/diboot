8f2823563cba538cfd2a4171e0a4f2e41b3d07cc
JerryMa <mazc@dibo.ltd>
2021-07-27 10:53:50 +0800

    * 优化AuthService多实现加载机制

--- diboot-iam-starter/src/main/java/com/diboot/iam/auth/AuthServiceFactory.java
+++ diboot-iam-starter/src/main/java/com/diboot/iam/auth/AuthServiceFactory.java
@@ -18,6 +18,7 @@ package com.diboot.iam.auth;
 import com.diboot.core.util.ContextHelper;
 import com.diboot.core.util.V;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 
 import java.util.HashMap;
 import java.util.List;
@@ -43,7 +44,14 @@ public class AuthServiceFactory {
             List<AuthService> authServiceList = ContextHelper.getBeans(AuthService.class);
             if(V.notEmpty(authServiceList)){
                 authServiceList.stream().forEach((service)->{
-                    AUTHTYPE_SERVICE_CACHE.put(service.getAuthType(), service);
+                    if(AUTHTYPE_SERVICE_CACHE.containsKey(service.getAuthType())){
+                        if(service.getClass().getAnnotation(Primary.class) != null){
+                            AUTHTYPE_SERVICE_CACHE.put(service.getAuthType(), service);
+                        }
+                    }
+                    else{
+                        AUTHTYPE_SERVICE_CACHE.put(service.getAuthType(), service);
+                    }
                 });
             }
         }
