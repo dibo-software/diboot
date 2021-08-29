a1a06e77458d3adfe71f96a853cdb87b4d9c218a
wind <1103642893@qq.com>
2021-08-29 16:15:29 +0800

    - 移除iam依赖

--- diboot-scheduler-starter/src/main/java/com/diboot/scheduler/vo/ScheduleJobVO.java
+++ diboot-scheduler-starter/src/main/java/com/diboot/scheduler/vo/ScheduleJobVO.java
@@ -15,9 +15,7 @@
  */
 package com.diboot.scheduler.vo;
 
-import com.diboot.core.binding.annotation.BindField;
 import com.diboot.core.config.Cons;
-import com.diboot.iam.entity.IamUser;
 import com.diboot.scheduler.entity.ScheduleJob;
 import lombok.Getter;
 import lombok.Setter;
@@ -42,11 +40,5 @@ public class ScheduleJobVO extends ScheduleJob {
      public String getJobStatusLabel(){
         return Cons.ENABLE_STATUS.getLabel(this.getJobStatus());
      }
-
-    /**
-     * 创建人姓名
-     */
-    @BindField(entity = IamUser.class, field = "realname", condition = "this.create_by=id")
-    private String createByName;
 
 }
