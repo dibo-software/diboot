3e1385ab364a66a38a6208c8c2c93518063edb28
wuy <1311695042@qq.com>
2021-11-22 21:06:25 +0800

    +: add Copyright

--- diboot-core/src/main/java/com/diboot/core/dto/CascaderDTO.java
+++ diboot-core/src/main/java/com/diboot/core/dto/CascaderDTO.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright (c) 2015-2021, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.diboot.core.dto;
 
-import com.diboot.core.entity.ValidList;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +25,13 @@ import java.util.Map;
 
 /**
  * 级联DTO
+ *
  * @author : uu
- * @version : v1.0
+ * @version : v2.4.0
  * @Date 2021/11/22  14:20
  */
-@Getter@Setter
+@Getter
+@Setter
 public class CascaderDTO implements Serializable {
     private static final long serialVersionUID = -3659986749020101170L;
 
--- diboot-core/src/main/java/com/diboot/core/service/CascaderService.java
+++ diboot-core/src/main/java/com/diboot/core/service/CascaderService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015-2021, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.diboot.core.service;
 
 import com.diboot.core.dto.CascaderDTO;
@@ -9,7 +24,7 @@ import java.util.List;
  * 级联Service，如需要，业务对象实现即可
  *
  * @author : uu
- * @version : v1.0
+ * @version : v2.4.0
  * @Date 2021/11/22  14:26
  */
 public interface CascaderService {
