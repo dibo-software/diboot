a57ad13f9da74eccfaa9dcf4bfbd22b04b575558
wuy <1311695042@qq.com>
2021-12-10 14:54:13 +0800

    *: 优化配置

--- diboot-mobile-ui/App.vue
+++ diboot-mobile-ui/App.vue
@@ -12,6 +12,31 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	@import "uview-ui/index.scss";
+
 	/*每个页面公共css */
+	page, .h100  {
+		height: 100%;
+	}
+	.page-bg-color {
+		background-color: $u-bg-color;
+	}
+	.di-scroll {
+		width: 100%;
+		height: 100%;
+		&-list {
+			background-color: #fff;
+			border-radius: 20rpx;
+			overflow: hidden;
+			margin: 20rpx;
+		}
+		&_create {
+			width: 100%;
+			position: fixed;
+			left: 0;
+			bottom: 100rpx;
+			padding: 0 24rpx;
+		}
+	}
 </style>
