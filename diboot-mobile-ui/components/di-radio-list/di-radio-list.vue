c22c7f803caa74c4e617423ab1a3b7622455c5a3
wuy <1311695042@qq.com>
2021-12-25 14:55:39 +0800

    + 移动端增加di-radio-list组件

---
+++ diboot-mobile-ui/components/di-radio-list/di-radio-list.vue
@@ -0,0 +1,50 @@
+<template>
+	<u-radio-group v-model="tempVal" :active-color="activeColor" @change="handleChange">
+		<u-radio v-for="(item, index) in list" :key="index" :name="item.value">
+			{{ item.label }}
+		</u-radio>
+	</u-radio-group>
+</template>
+
+<script>
+	export default {
+		name:"di-radio-list",
+		data() {
+			return {
+				tempVal: this.value
+			};
+		},
+		methods: {
+			/**
+			 * 切换后触发
+			 * @param {Object} val
+			 */
+			handleChange(val) {
+				this.$emit('input', val)
+			}
+		},
+		watch: {
+			value(value) {
+				this.tempVal = value
+			}
+		},
+		props: {
+			value: {
+				type: [String, Boolean],
+				require: true
+			},
+			list: {
+				type: Array,
+				require: true
+			},
+			activeColor: {
+				type: String,
+				default: '#19be6b'
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
