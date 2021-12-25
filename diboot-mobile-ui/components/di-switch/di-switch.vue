72da2d0635719a2c40f25c6a77abab05147b8902
wuy <1311695042@qq.com>
2021-12-25 14:56:10 +0800

    + 移动端增加di-switch组件

---
+++ diboot-mobile-ui/components/di-switch/di-switch.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="di-switch">
+		<u-switch v-model="tempVal" active-color="#19be6b"></u-switch>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"di-switch",
+		data() {
+			return {
+				tempVal: this.value
+			};
+		},
+		mounted() {
+			// 设置初始化值
+			this.$nextTick(function(){
+				this.$emit('input', this.value || false)
+			})
+		},
+		watch: {
+			tempVal(val) {
+				this.$emit('input', val)
+			},
+			//回显
+			value(val) {
+				this.tempVal = val
+			}
+		},
+		props: {
+			value: {
+				type: Boolean,
+				default: false
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
