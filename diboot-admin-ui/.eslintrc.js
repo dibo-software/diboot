8cf1b83d3ae7abfc681ad9f89b0971a9c3bc655c
wind <1103642893@qq.com>
2022-04-19 14:30:17 +0800

    + prettier

--- diboot-admin-ui/.eslintrc.js
+++ diboot-admin-ui/.eslintrc.js
@@ -1,30 +1,38 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    parser: "vue-eslint-parser",
-    extends: [
-        "eslint:recommended",
-        "plugin:vue/vue3-recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        parser: "@typescript-eslint/parser",
-        sourceType: "module"
-    },
-    plugins: [
-        "vue",
-        "@typescript-eslint"
-    ],
-    globals: {
-        defineProps: 'readonly',
-        defineEmits: 'readonly',
-        defineExpose: 'readonly',
-        withDefaults: 'readonly',
-    },
-    rules: {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parser: 'vue-eslint-parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+    'comma-dangle': ['warn', 'only-multiline'],
+    'vue/html-self-closing': ['warn', { html: { void: 'always' } }],
+    'no-undef': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'warn',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off'
+  }
 }
--- diboot-admin-ui/src/components/HelloWorld.vue
+++ diboot-admin-ui/src/components/HelloWorld.vue
@@ -19,9 +19,7 @@ const count = ref(0)
   <p>See <code>README.md</code> for more information.</p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
