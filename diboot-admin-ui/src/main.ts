efb8d31f20a6abd365e5c60c9e8af68a71298916
wind <1103642893@qq.com>
2022-04-20 18:13:02 +0800

    + pinia

--- diboot-admin-ui/src/main.ts
+++ diboot-admin-ui/src/main.ts
@@ -3,4 +3,5 @@ import router from './router'
 
 const app = createApp(App)
 app.use(router)
+app.use(createPinia())
 app.mount('#app')
--- diboot-admin-ui/vite.config.ts
+++ diboot-admin-ui/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       // 解析器
       resolvers: [ElementPlusResolver()],
       // 自动导入Api
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', 'vue-router', 'pinia'],
       // 为true时在项目根目录自动创建
       dts: 'types/auto-imports.d.ts'
     }),
