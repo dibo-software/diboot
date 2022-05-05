d5e0960a4e2848fa86605a52ce5e17ce673d4f7b
wind <1103642893@qq.com>
2022-05-05 11:23:09 +0800

    * style menu background-color

--- diboot-admin-ui/src/layout/index.vue
+++ diboot-admin-ui/src/layout/index.vue
@@ -173,6 +173,11 @@ const isMenuCollapse = ref(false)
   -webkit-transition: width 0.3s;
 }
 
+.el-sub-menu.is-active,
+.el-menu-item.is-active {
+  background-color: var(--el-color-primary-light-9) !important;
+}
+
 .subfield {
   display: flex;
 
@@ -216,8 +221,10 @@ const isMenuCollapse = ref(false)
     }
   }
 }
+
 .default-menu {
   border: none;
+
   .default-menu-item {
     height: 70px !important;
     width: 70px;
@@ -227,6 +234,7 @@ const isMenuCollapse = ref(false)
     align-items: center;
     text-align: center;
     justify-content: center;
+
     .title {
       line-height: 24px;
       font-size: 14px;
