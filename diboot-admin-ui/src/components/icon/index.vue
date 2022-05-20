d2d0bb5698e97b6a8ba96c3b0e9281de5c12aff8
wuy <1311695042@qq.com>
2022-05-20 13:44:18 +0800

    * 优化icon组件

--- diboot-admin-ui/src/components/icon/index.vue
+++ diboot-admin-ui/src/components/icon/index.vue
@@ -3,23 +3,13 @@ import IconLibrary from '@/config/iconLibrary'
 /**
  * 图标名称 [库别名（不填默认为Element）]:[(路径)图标名]
  */
-const props = withDefaults(
-  defineProps<{
-    name?: string
-    size?: number
-    color?: string
-  }>(),
-  {
-    name: '',
-    color: 'inherit',
-    size: 20
-  }
-)
+const props = defineProps<{
+  name?: string
+}>()
 
 const Icon = computed(() => {
   if (!props.name) return
   const split = props.name.includes(':') ? props.name.split(/:/) : ['Element', props.name]
-  console.log(split)
   const lib = IconLibrary[split[0]]
   return lib && lib[split[1]]
 })
@@ -27,8 +17,6 @@ const Icon = computed(() => {
 
 <template>
   <template v-if="Icon">
-    <el-icon :size="size" :color="color">
-      <icon />
-    </el-icon>
+    <icon />
   </template>
 </template>
