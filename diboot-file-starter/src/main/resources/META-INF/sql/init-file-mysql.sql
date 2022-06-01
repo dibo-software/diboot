01bc778d0969b345cb6db514e311cb2c6684eef7
wind <1103642893@qq.com>
2022-06-01 19:16:07 +0800

    * 重构文件组件

--- diboot-file-starter/src/main/resources/META-INF/sql/init-file-mysql.sql
+++ diboot-file-starter/src/main/resources/META-INF/sql/init-file-mysql.sql
@@ -1,21 +1,21 @@
--- 上传文件表
-CREATE TABLE upload_file (
-  uuid varchar(32) NOT NULL COMMENT '编号' primary key,
-  tenant_id bigint NOT NULL DEFAULT 0 COMMENT '租户ID',
-  app_module  varchar(50)   null comment '应用模块',
-  rel_obj_type varchar(50) DEFAULT NULL COMMENT '关联对象类',
-  rel_obj_id varchar(32) DEFAULT NULL COMMENT '关联对象ID',
-  rel_obj_field varchar(50) DEFAULT NULL COMMENT '关联对象属性名称',
-  file_name varchar(100) NOT NULL COMMENT '文件名',
-  storage_path varchar(200) NOT NULL COMMENT '存储路径',
-  access_url varchar(200) NULL COMMENT '访问地址',
-  file_type varchar(20) DEFAULT NULL COMMENT '文件类型',
-  data_count int DEFAULT 0 COMMENT '数据量',
-  description varchar(100) DEFAULT NULL COMMENT '备注',
-  is_deleted   tinyint(1)  default 0                 not null comment '是否删除',
-  create_by    bigint      default 0 COMMENT '创建人',
-  create_time  datetime   default CURRENT_TIMESTAMP not null comment '创建时间'
-) DEFAULT CHARSET=utf8 COMMENT='上传文件';
+-- 文件记录
+create table file_record
+(
+    uuid          varchar(32)                        not null comment 'ID' primary key,
+    tenant_id     bigint   default 0                 not null comment '租户ID',
+    app_module    varchar(50) null comment '应用模块',
+    md5           varchar(32) null comment 'MD5标识',
+    file_name     varchar(100)                       not null comment '文件名称',
+    file_type     varchar(20) null comment '文件类型',
+    file_size     bigint                             not null comment '文件大小',
+    storage_path  varchar(200)                       not null comment '存储路径',
+    access_url    varchar(200) null comment '访问地址',
+    thumbnail_url varchar(200) null comment '缩略图地址',
+    description   varchar(100) null comment '备注',
+    is_deleted    tinyint(1) default 0 not null comment '删除标记',
+    create_by     bigint   default 0 null comment '创建人',
+    create_time   datetime default CURRENT_TIMESTAMP not null comment '创建时间'
+) comment '文件存储' charset = utf8mb4;
 -- 索引
-create index idx_upload_file on upload_file (rel_obj_type, rel_obj_id, rel_obj_field);
-create index idx_upload_file_tenant on upload_file (tenant_id);
+create index idx_file_record_md5 on file_record (md5);
+create index idx_file_record_tenant on file_record (tenant_id);
