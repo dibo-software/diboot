d914fd1314f7703507951d6b8a376252cf5a5e13
JerryMa <mazc@dibo.ltd>
2022-06-06 09:41:15 +0800

    * 移除isOK

--- diboot-core/src/main/java/com/diboot/core/vo/JsonResult.java
+++ diboot-core/src/main/java/com/diboot/core/vo/JsonResult.java
@@ -202,14 +202,6 @@ public class JsonResult<T> implements Serializable {
         }
     }
 
-    /**
-     * 判断结果是否OK
-     * @return
-     */
-    public boolean isOK(){
-        return this.code == Status.OK.code();
-    }
-
     /***
      * 请求处理成功
      */
