0f90f0491b214a98b50a127e2578ccfde1561766
wuy <1311695042@qq.com>
2022-06-09 16:07:38 +0800

    *（admin-ui）优化配置

--- diboot-admin-ui/vite.config.ts
+++ diboot-admin-ui/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
-import checker from 'vite-plugin-checker'
+// import checker from 'vite-plugin-checker'
 import eslintPlugin from 'vite-plugin-eslint'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -15,7 +15,7 @@ export default defineConfig(({ command }) => {
     plugins: [
       vue(),
       VueSetupExtend(),
-      checker({ vueTsc: true }),
+      // checker({ vueTsc: true }),
       eslintPlugin({ fix: true }),
       AutoImport({
         // 解析器
