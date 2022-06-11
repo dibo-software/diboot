9a5cb48476c8a67a5b7188d6bd27c76d3a85d8de
wind <1103642893@qq.com>
2022-06-11 23:11:41 +0800

    * (admin-ui) 优化示例样式

--- diboot-admin-ui/src/views/example/richText.vue
+++ diboot-admin-ui/src/views/example/richText.vue
@@ -8,29 +8,18 @@ const doc = ref(false)
 </script>
 
 <template>
-  <div style="height: calc(100% - 20px); padding: 10px">
-    <el-row>
-      <el-col :span="18">
-        <el-alert type="success" :closable="false">
-          <span style="font-size: 13px">感谢优秀的 `wangEditor`，项目地址：</span>
-          <el-link type="primary" href="https://github.com/wangeditor-team/wangEditor">
-            https://github.com/wangeditor-team/wangEditor
-          </el-link>
-        </el-alert>
-      </el-col>
-      <el-col :span="6">
-        <el-button type="primary" style="position: absolute; right: 15px" @click="doc = !doc">
-          切换为：{{ doc ? '富文本编辑器' : '文档编辑器' }}
-        </el-button>
-      </el-col>
-    </el-row>
+  <div style="height: calc(100% - 20px); padding: 10px; display: flex; flex-direction: column">
+    <el-space style="justify-content: space-between; margin-bottom: 5px">
+      <el-alert type="success" :closable="false">
+        <span style="font-size: 13px">感谢优秀的 `wangEditor`，项目地址：</span>
+        <el-link type="primary" href="https://github.com/wangeditor-team/wangEditor">
+          https://github.com/wangeditor-team/wangEditor
+        </el-link>
+      </el-alert>
+      <el-button type="primary" @click="doc = !doc"> 切换为：{{ doc ? '富文本编辑器' : '文档编辑器' }} </el-button>
+    </el-space>
     <!-- 富文本编辑器 -->
-    <rich-editor
-      v-model="value"
-      v-model:title="title"
-      :doc="doc && 'title'"
-      style="height: calc(100% - 41px); margin-top: 10px"
-    />
+    <rich-editor v-model="value" v-model:title="title" :doc="doc && 'title'" style="flex: 1" />
   </div>
 </template>
 
