521c7094a8f2c4bf070a3dd1728ee932fb7ba94e
wind <1103642893@qq.com>
2022-06-16 18:09:28 +0800

    + license header

--- diboot-message-starter/src/main/java/com/diboot/message/annotation/BindVariable.java
+++ diboot-message-starter/src/main/java/com/diboot/message/annotation/BindVariable.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015-2022, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.diboot.message.annotation;
 
 import java.lang.annotation.*;
@@ -5,7 +20,7 @@ import java.lang.annotation.*;
 /**
  * 绑定变量
  * @author JerryMa
- * @version v3.0.0
+ * @version v2.6.0
  * @date 2022/5/27
  * Copyright © diboot.com
  */
