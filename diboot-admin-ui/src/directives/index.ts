831833780bff0172fb65ff8da46f17832c5f9c17
JerryMa <mazc@dibo.ltd>
2022-07-01 17:17:16 +0800

    * 更新配套前端说明文档

--- diboot-admin-ui/src/directives/index.ts
+++
@@ -1,7 +0,0 @@
-import { App } from 'vue'
-import { hasRole, hasPermission } from './permission'
-
-export default (app: App) => {
-  app.directive('hasRole', hasRole)
-  app.directive('hasPermission', hasPermission)
-}
