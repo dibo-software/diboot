7ac757f910522ab1c87b186780a3b4d4797cbd88
wind <1103642893@qq.com>
2022-07-18 21:52:43 +0800

    * 优化通用前端选项获取

--- diboot-core/src/main/java/com/diboot/core/controller/BaseController.java
+++ diboot-core/src/main/java/com/diboot/core/controller/BaseController.java
@@ -25,11 +25,9 @@ import com.diboot.core.binding.helper.WrapperHelper;
 import com.diboot.core.binding.parser.PropInfo;
 import com.diboot.core.config.BaseConfig;
 import com.diboot.core.config.Cons;
-import com.diboot.core.dto.AttachMoreDTO;
-import com.diboot.core.entity.ValidList;
+import com.diboot.core.dto.RelatedDataDTO;
 import com.diboot.core.exception.BusinessException;
 import com.diboot.core.service.BaseService;
-import com.diboot.core.service.DictionaryService;
 import com.diboot.core.util.ContextHelper;
 import com.diboot.core.util.JSON;
 import com.diboot.core.util.S;
@@ -54,12 +52,6 @@ public class BaseController {
 
 	@Autowired
 	protected HttpServletRequest request;
-
-	/**
-	 * 字典service
-	 */
-	@Autowired(required = false)
-	protected DictionaryService dictionaryService;
 
 	/***
 	 * 根据DTO构建查询QueryWrapper (根据BindQuery注解构建相应的查询条件，DTO中的非空属性均参与构建)
@@ -219,110 +211,84 @@ public class BaseController {
 	}
 
 	/**
-	 * 通用的attachMore获取数据
+	 * RelatedData获取相应对象数据
 	 *
-	 * @param attachMoreDTOList
-	 * @return
-	 */
-	protected Map<String, List<LabelValue>> attachMoreRelatedData(ValidList<AttachMoreDTO> attachMoreDTOList) {
-		if (V.isEmpty(attachMoreDTOList)) {
-			return Collections.emptyMap();
-		}
-		Map<String, List<LabelValue>> result = new HashMap<>(attachMoreDTOList.size());
-		for (AttachMoreDTO attachMoreDTO : attachMoreDTOList) {
-			// 请求参数安全检查
-			V.securityCheck(attachMoreDTO.getTarget(), attachMoreDTO.getValue(), attachMoreDTO.getLabel(), attachMoreDTO.getExt());
-			result.computeIfAbsent(S.getIfEmpty(attachMoreDTO.getAlias(), () -> S.toLowerCaseCamel(attachMoreDTO.getTarget()) + "Options"),
-					key -> V.isEmpty(attachMoreDTO.getLabel()) ? attachMoreRelatedData(attachMoreDTO.getTarget()) : attachMoreRelatedData(attachMoreDTO));
-		}
-		return result;
-	}
-
-	/**
-	 * 获取字典指定类型的元素集合
-	 *
-	 * @param dictType 字典类型
+	 * @param relatedDataDTO
 	 * @return labelValue集合
 	 */
-	protected List<LabelValue> attachMoreRelatedData(String dictType) {
-		return dictionaryService.getLabelValueList(dictType);
-	}
-
-	/**
-	 * 通用的attachMore获取相应对象数据
-	 *
-	 * @param attachMoreDTO
-	 * @return labelValue集合
-	 */
-	protected List<LabelValue> attachMoreRelatedData(AttachMoreDTO attachMoreDTO) {
-		if (!attachMoreDTO.isTree()) {
-			attachMoreDTO.setParent(null);
+	protected List<LabelValue> loadRelatedData(RelatedDataDTO relatedDataDTO) {
+		if (!relatedDataDTO.isTree()) {
+			relatedDataDTO.setParent(null);
 		}
-		return attachMoreRelatedData(attachMoreDTO, null, null);
+		return loadRelatedData(relatedDataDTO, null, null, null);
 	}
 
 	/**
-	 * 获取attachMore指定层级的数据集合
+	 * 获取RelatedData指定层级的数据集合
 	 * <p>
 	 * 用于异步加树形数据
 	 *
-	 * @param attachMoreDTO
+	 * @param relatedDataDTO
+	 * @param keyword       模糊搜索关键字
 	 * @param parentValue   父级值
 	 * @param parentType    父级类型
 	 * @return labelValue集合
 	 */
-	protected List<LabelValue> attachMoreRelatedData(AttachMoreDTO attachMoreDTO, String parentValue, String parentType) {
+	protected List<LabelValue> loadRelatedData(RelatedDataDTO relatedDataDTO, String keyword,
+											   String parentValue, String parentType) {
 		if (V.notEmpty(parentType)) {
-			attachMoreDTO = findAttachMoreByType(attachMoreDTO, parentType);
-			if (attachMoreDTO != null && !attachMoreDTO.isTree()) {
-				attachMoreDTO = attachMoreDTO.getNext().setTree(false);
+			relatedDataDTO = findRelatedDataByType(relatedDataDTO, parentType);
+			if (relatedDataDTO != null && !relatedDataDTO.isTree()) {
+				relatedDataDTO = relatedDataDTO.getNext().setTree(false);
 			}
 		}
-		if (attachMoreDTO == null) {
+		if (relatedDataDTO == null) {
 			return Collections.emptyList();
 		}
-		List<LabelValue> labelValueList = attachMoreRelatedData(attachMoreDTO, parentValue);
-		if (attachMoreDTO.isTree() && attachMoreDTO.getNext() != null) {
-			labelValueList.addAll(attachMoreRelatedData(attachMoreDTO.getNext().setTree(false), parentValue));
+		List<LabelValue> labelValueList = loadRelatedData(relatedDataDTO, parentValue, keyword);
+		if (relatedDataDTO.isTree() && relatedDataDTO.getNext() != null) {
+			labelValueList.addAll(loadRelatedData(relatedDataDTO.getNext().setTree(false), parentValue, keyword));
 		}
 		return labelValueList;
 	}
 
 	/**
-	 * 根据type查找AttachMore
+	 * 根据type查找 RelatedDataDTO
 	 *
-	 * @param attachMore
-	 * @param type       类型，与attachMore.target对应
-	 * @return labelValue集合
+	 * @param relatedData
+	 * @param type        类型
+	 * @return RelatedDataDTO
 	 */
-	private AttachMoreDTO findAttachMoreByType(AttachMoreDTO attachMore, String type) {
-		return attachMore.getTarget().equals(type) ? attachMore
-				: (attachMore.getNext() == null ? null : findAttachMoreByType(attachMore.getNext(), type));
+	private RelatedDataDTO findRelatedDataByType(RelatedDataDTO relatedData, String type) {
+		return relatedData.getType().equals(type) ? relatedData
+				: (relatedData.getNext() == null ? null : findRelatedDataByType(relatedData.getNext(), type));
 	}
 
 	/**
-	 * 通用的attachMore获取数据
+	 * 通用的RelatedData获取数据
 	 *
-	 * @param attachMore  相应的attachMore
+	 * @param relatedDataDTO  相应的relatedDataDTO
 	 * @param parentValue 父级值
 	 * @return labelValue集合
 	 */
-	protected List<LabelValue> attachMoreRelatedData(AttachMoreDTO attachMore, String parentValue) {
-		if (!attachMoreSecurityCheck(attachMore)) {
-			log.warn("attachMore安全检查不通过: {}", JSON.stringify(attachMore));
-			return null;
+	private List<LabelValue> loadRelatedData(RelatedDataDTO relatedDataDTO, String parentValue, String keyword) {
+		V.securityCheck(relatedDataDTO.getType(), relatedDataDTO.getValue(), relatedDataDTO.getLabel(),
+				relatedDataDTO.getExt(), parentValue, keyword);
+		if (!relatedDataSecurityCheck(relatedDataDTO)) {
+			log.warn("relatedData安全检查不通过: {}", JSON.stringify(relatedDataDTO));
+			return Collections.emptyList();
 		}
-		if (V.notEmpty(parentValue) && !attachMore.isTree() && V.isEmpty(attachMore.getParent())) {
-			throw new BusinessException("attachMore跨表级联中的 " + attachMore.getTarget() + " 未指定关联父级属性 parent: ?");
+		if (V.notEmpty(parentValue) && !relatedDataDTO.isTree() && V.isEmpty(relatedDataDTO.getParent())) {
+			throw new BusinessException("relatedData跨表级联中的 " + relatedDataDTO.getType() + " 未指定关联父级属性 parent: ?");
 		}
-		String entityClassName = attachMore.getTargetClassName();
+		String entityClassName = relatedDataDTO.getTypeClassName();
 		Class<?> entityClass = BindingCacheManager.getEntityClassBySimpleName(entityClassName);
 		if (V.isEmpty(entityClass)) {
-			throw new BusinessException("attachMore: " + attachMore.getTarget() + " 不存在");
+			throw new BusinessException("relatedData: " + relatedDataDTO.getType() + " 不存在");
 		}
 		BaseService<?> baseService = ContextHelper.getBaseServiceByEntity(entityClass);
 		if (baseService == null) {
-			throw new BusinessException("attachMore: " + attachMore.getTarget() + " 的Service不存在 ");
+			throw new BusinessException("relatedData: " + relatedDataDTO.getType() + " 的Service不存在 ");
 		}
 		PropInfo propInfoCache = BindingCacheManager.getPropInfoByClass(entityClass);
 		Function<String, String> field2column = field -> {
@@ -331,23 +297,23 @@ public class BaseController {
 				if (V.notEmpty(column)) {
 					return column;
 				} else {
-					throw new BusinessException("attachMore: " + attachMore.getTarget() + " 无 `" + field + "` 属性");
+					throw new BusinessException("relatedData: " + relatedDataDTO.getType() + " 无 `" + field + "` 属性");
 				}
 			}
 			return null;
 		};
-		String label = field2column.apply(S.defaultIfBlank(attachMore.getLabel(), "label"));
-		String value = S.defaultString(field2column.apply(attachMore.getValue()), propInfoCache.getIdColumn());
-		String ext = field2column.apply(attachMore.getExt());
+		String label = field2column.apply(S.defaultIfBlank(relatedDataDTO.getLabel(), "label"));
+		String value = S.defaultString(field2column.apply(relatedDataDTO.getValue()), propInfoCache.getIdColumn());
+		String ext = field2column.apply(relatedDataDTO.getExt());
 
 		// 构建查询条件
 		QueryWrapper<?> queryWrapper = Wrappers.query()
 				.select(V.isEmpty(ext) ? new String[]{label, value} : new String[]{label, value, ext})
-				.like(V.notEmpty(attachMore.getKeyword()), label, attachMore.getKeyword());
+				.like(V.notEmpty(keyword), label, keyword);
 		// 构建排序
-        WrapperHelper.buildOrderBy(queryWrapper, attachMore.getOrderBy(), field2column);
+        WrapperHelper.buildOrderBy(queryWrapper, relatedDataDTO.getOrderBy(), field2column);
 		// 父级限制
-		String parentColumn = field2column.apply(S.defaultIfBlank(attachMore.getParent(), attachMore.isTree() ? "parentId" : null));
+		String parentColumn = field2column.apply(S.defaultIfBlank(relatedDataDTO.getParent(), relatedDataDTO.isTree() ? "parentId" : null));
 		if (V.notEmpty(parentColumn)) {
 			if (V.notEmpty(parentValue)) {
 				queryWrapper.eq(parentColumn, parentValue);
@@ -356,24 +322,24 @@ public class BaseController {
 			}
 		}
 		// 构建附加条件
-		buildAttachMoreCondition(attachMore, queryWrapper, field2column);
+		buildRelatedDataCondition(relatedDataDTO, queryWrapper, field2column);
 		// 获取数据并做相应填充
 		List<LabelValue> labelValueList = baseService.getLabelValueList(queryWrapper);
-		if (V.isEmpty(attachMore.getKeyword()) && labelValueList.size() > BaseConfig.getBatchSize()) {
-			log.warn("attachMore: {} 数据量超过 {} 条, 建议采用远程搜索接口过滤数据", entityClassName, BaseConfig.getBatchSize());
+		if (V.isEmpty(keyword) && labelValueList.size() > BaseConfig.getBatchSize()) {
+			log.warn("relatedData: {} 数据量超过 {} 条, 建议采用远程搜索接口过滤数据", entityClassName, BaseConfig.getBatchSize());
 		}
-		if (V.notEmpty(parentColumn) || attachMore.getNext() != null) {
-			Boolean leaf = !attachMore.isTree() && attachMore.getNext() == null ? true : null;
+		if (V.notEmpty(parentColumn) || relatedDataDTO.getNext() != null) {
+			Boolean leaf = !relatedDataDTO.isTree() && relatedDataDTO.getNext() == null ? true : null;
 			// 第一层tree与最后一层无需返回type
-			String type = attachMore.isTree() || Boolean.TRUE.equals(leaf) ? null : attachMore.getTarget();
+			String type = relatedDataDTO.isTree() || Boolean.TRUE.equals(leaf) ? null : relatedDataDTO.getType();
 			labelValueList.forEach(item -> {
 				item.setType(type);
 				item.setLeaf(leaf);
 				// 非异步加载
-				if (!Boolean.TRUE.equals(leaf) && !attachMore.isLazy()) {
-					List<LabelValue> children = attachMoreRelatedData(attachMore, S.valueOf(item.getValue()), type);
+				if (!Boolean.TRUE.equals(leaf) && !relatedDataDTO.isLazy()) {
+					List<LabelValue> children = loadRelatedData(relatedDataDTO, keyword, S.valueOf(item.getValue()), type);
 					item.setChildren(children.isEmpty() ? null : children);
-					item.setDisabled(children.isEmpty() && attachMore.getNext() != null ? true : null);
+					item.setDisabled(children.isEmpty() && relatedDataDTO.getNext() != null ? true : null);
 				}
 			});
 		}
@@ -381,27 +347,27 @@ public class BaseController {
 	}
 
     /**
-     * attachMore 安全检查
+     * relatedData 安全检查
      *
-     * @param attachMore
+     * @param relatedDataDTO
      * @return 是否允许访问该类型接口
      */
-    protected boolean attachMoreSecurityCheck(AttachMoreDTO attachMore) {
+    protected boolean relatedDataSecurityCheck(RelatedDataDTO relatedDataDTO) {
 		return true;
     }
 
 	/**
-	 * 构建AttachMore的筛选条件
+	 * 构建RelatedData的筛选条件
 	 *
-	 * @param attachMore
+	 * @param relatedDataDTO
 	 * @param queryWrapper
-	 * @param field2column attachMore.target对象的属性名转列名
+	 * @param field2column relatedData.type对象的属性名转列名
 	 */
-	protected void buildAttachMoreCondition(AttachMoreDTO attachMore, QueryWrapper<?> queryWrapper, Function<String, String> field2column) {
-		if (attachMore.getCondition() == null) {
+	protected void buildRelatedDataCondition(RelatedDataDTO relatedDataDTO, QueryWrapper<?> queryWrapper, Function<String, String> field2column) {
+		if (relatedDataDTO.getCondition() == null) {
 			return;
 		}
-		for (Map.Entry<String, Object> item : attachMore.getCondition().entrySet()) {
+		for (Map.Entry<String, Object> item : relatedDataDTO.getCondition().entrySet()) {
 			String columnName = field2column.apply(item.getKey());
 			Object itemValue = item.getValue();
 			if (itemValue == null) {
--- diboot-core/src/main/java/com/diboot/core/dto/RelatedDataDTO.java
+++ diboot-core/src/main/java/com/diboot/core/dto/RelatedDataDTO.java
@@ -2,7 +2,6 @@ package com.diboot.core.dto;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.diboot.core.controller.BaseController;
-import com.diboot.core.entity.Dictionary;
 import com.diboot.core.util.S;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
@@ -15,10 +14,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 /**
- * attachMore 用于加载关联数据传递的DTO格式
- * <p>
- * [{target: 'Category', label: 'name’, value: 'id'}, {target: 'GENDER'}]
- * </p>
+ * 用于加载关联数据传递的DTO格式
  *
  * @author mazc@dibo.ltd
  * @version v2.0
@@ -27,73 +23,37 @@ import java.util.function.Function;
 @Getter
 @Setter
 @Accessors(chain = true)
-public class AttachMoreDTO implements Serializable {
+public class RelatedDataDTO implements Serializable {
     private static final long serialVersionUID = 10301L;
 
     /**
-     * 关联类型
+     * <h3>需要查询的目标对象类型</h3>
      */
-    @Deprecated
-    public enum REF_TYPE {
-        /**
-         * 绑定的是对象
-         */
-        T,
-        /**
-         * 绑定的是字典
-         */
-        D
-    }
-
-    /**
-     * 关联的类型
-     */
-    @Deprecated
-    private REF_TYPE type;
-
-    /**
-     * <h3>需要查询的目标数据<br/>当label 为空时，则为获取字典，否则获取对象</h3>
-     * target应为 实体名 或 字典的type{@link Dictionary#type}
-     */
-    @NotNull(message = "查询类型不能为空！")
-    private String target;
-
-    /**
-     * <h3>别名</h3>
-     * 仅在批量获取选项时生效
-     */
-    private String alias;
+    @NotNull(message = "类型不能为空！")
+    private String type;
 
     /**
      * <h3>需要查询的label字段</h3>
-     * 当为空时，则为获取字典，label为{@link Dictionary#itemName}
      */
+    @NotNull(message = "label不能为空！")
     private String label;
 
     /**
      * <h3>需要查询的value字段</h3>
-     * 当获取对象时，value默认为主键字段；<br/>
-     * 当获取字典时，value为{@link Dictionary#itemValue}
+     * 默认为主键字段
      */
     private String value;
 
     /**
      * <h3>需要查询的ext字段</h3>
-     * 当获取字典时，ext为表中{@link Dictionary#extdata}
      */
     private String ext;
 
     /**
      * <h3>筛选条件</h3>
-     * 可重写{@link BaseController#buildAttachMoreCondition(AttachMoreDTO, QueryWrapper, Function)}进行自定义筛选条件规则
+     * 可重写{@link BaseController#buildRelatedDataCondition(RelatedDataDTO, QueryWrapper, Function)}进行自定义筛选条件规则
      */
     private Map<String, Object> condition;
-
-    /**
-     * <h3>关键字</h3>
-     * 用于前端远程搜索label
-     */
-    private String keyword;
 
     /**
      * <h3>排序</h3>
@@ -122,11 +82,11 @@ public class AttachMoreDTO implements Serializable {
     /**
      * <h3>下一层</h3>
      */
-    private AttachMoreDTO next;
+    private RelatedDataDTO next;
 
     @JsonIgnore
-    public String getTargetClassName(){
-        return S.capFirst(S.toLowerCaseCamel(this.target));
+    public String getTypeClassName(){
+        return S.capFirst(S.toLowerCaseCamel(this.type));
     }
 
 }
