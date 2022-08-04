374643ca41f2a8c395f232f4c743eddf25442b76
JerryMa <mazc@dibo.ltd>
2022-08-04 15:45:41 +0800

    + SqlDate -> LocalDate* 转换器

---
+++ diboot-core/src/main/java/com/diboot/core/converter/SqlDate2LocalDateConverter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015-2029, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.diboot.core.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+/**
+ * java.sql.Date - LocalDate 转换器
+ * @author JerryMa
+ * @version v2.6.0
+ * @date 2022/5/11
+ * Copyright © diboot.com
+ */
+public class SqlDate2LocalDateConverter implements Converter<Date, LocalDate> {
+
+    @Override
+    public LocalDate convert(Date source) {
+        if (source == null) {
+            return null;
+        }
+        return source.toLocalDate();
+    }
+
+}
