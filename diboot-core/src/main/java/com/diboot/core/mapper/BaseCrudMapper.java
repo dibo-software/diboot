4caf5d22c1698d9405c415c1e617cccdf60b8c33
wind <1103642893@qq.com>
2022-10-13 17:19:19 +0800

    - 移除BaseCrudMapper中通过id撤回删除状态

--- diboot-core/src/main/java/com/diboot/core/mapper/BaseCrudMapper.java
+++ diboot-core/src/main/java/com/diboot/core/mapper/BaseCrudMapper.java
@@ -16,10 +16,6 @@
 package com.diboot.core.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Update;
-
-import java.io.Serializable;
 
 /**
  * 基础CRUD的父类Mapper
@@ -29,12 +25,4 @@ import java.io.Serializable;
  */
 public interface BaseCrudMapper<T> extends BaseMapper<T> {
 
-    /***
-     * 通过id撤回当前记录的删除状态
-     * @param tableName
-     * @param id
-     * @return
-     */
-    @Update("UPDATE `${tableName}` SET is_deleted=0 WHERE id=#{id}")
-    int cancelDeletedById(@Param("tableName") String tableName, @Param("id") Serializable id);
 }
