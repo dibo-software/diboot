b5665026542ae365afe57beb8e81247d879aa57b
JerryMa <mazc@dibo.ltd>
2022-11-02 12:39:28 +0800

    * 加setter以支持反序列化

--- diboot-core/src/main/java/com/diboot/core/vo/JsonResult.java
+++ diboot-core/src/main/java/com/diboot/core/vo/JsonResult.java
@@ -177,6 +177,15 @@ public class JsonResult<T> implements Serializable {
     public T getData() {
         return filterJsonResultData(data);
     }
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
 
     /***
      * 绑定分页信息
