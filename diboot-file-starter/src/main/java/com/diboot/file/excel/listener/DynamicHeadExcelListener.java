b7536ec61c37142965e91f91b07e799e73fc0eb0
JerryMa <mazc@dibo.ltd>
2022-11-03 16:51:34 +0800

    * 优化DynamicHeadExcelListener支持动态

--- diboot-file-starter/src/main/java/com/diboot/file/excel/listener/DynamicHeadExcelListener.java
+++ diboot-file-starter/src/main/java/com/diboot/file/excel/listener/DynamicHeadExcelListener.java
@@ -17,12 +17,15 @@ package com.diboot.file.excel.listener;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.metadata.Head;
+import com.alibaba.excel.metadata.data.ReadCellData;
+import com.alibaba.excel.read.metadata.property.ExcelReadHeadProperty;
+import com.diboot.file.util.ExcelHelper;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /***
  * 非固定表头的excel数据读取listener
@@ -32,12 +35,47 @@ import java.util.Map;
 @Slf4j
 public abstract class DynamicHeadExcelListener extends AnalysisEventListener<Map<Integer, String>> {
     // 表头
-    Map<Integer, String> headMap = null;
+    @Getter
+    Map<Integer, String> headMap = new HashMap<>();
+    /**
+     * 是否为预览模式
+     */
+    @Setter
+    protected boolean preview = false;
     /**
      * 注入request
      */
     @Setter
     private Map<String, Object> requestParams;
+
+    /**
+     * 属性名映射
+     */
+    @Getter
+    private final HashMap<Integer, String> fieldNameMap = new HashMap<>();
+    /**
+     * 列名映射
+     */
+    @Getter
+    private final TreeMap<Integer, List<String>> headNameMap = new TreeMap<>();
+
+    /**
+     * 预览数据
+     */
+    @Getter @Setter
+    private List<Map<Integer, String>> previewDataList;
+
+    @Getter
+    private Integer totalCount = 0;
+
+    /**
+     * 错误信息
+     */
+    @Getter
+    private List<String> errorMsgs;
+    @Getter
+    protected Integer errorCount = 0;
+
     // 全部数据
     private List<Map<Integer, String>> dataList = new ArrayList<>();
 
@@ -46,11 +84,38 @@ public abstract class DynamicHeadExcelListener extends AnalysisEventListener<Map
         dataList.add(data);
     }
 
+
+    /**
+     * excel表头数据
+     **/
+    @Override
+    public void invokeHead(Map<Integer, ReadCellData<?>> headMap, AnalysisContext context) {
+        this.headMap.clear();
+        fieldNameMap.clear();
+        headNameMap.clear();
+        ExcelReadHeadProperty excelReadHeadProperty = context.currentReadHolder().excelReadHeadProperty();
+        for (Map.Entry<Integer, Head> entry : excelReadHeadProperty.getHeadMap().entrySet()) {
+            Integer index = entry.getKey();
+            Head head = entry.getValue();
+            String fieldName = head.getFieldName();
+            List<String> headNameList = head.getHeadNameList();
+            String name = headNameList.get(headNameList.size() - 1);
+            this.headMap.put(index, name);
+            fieldNameMap.put(index, fieldName);
+            headNameMap.put(index, headNameList);
+        }
+    }
+
     @Override
     public void doAfterAllAnalysed(AnalysisContext context) {
         // 自定义校验
         additionalValidate(dataList, requestParams);
-        saveData(this.headMap, this.dataList, requestParams);
+        // TODO 检查
+        this.totalCount = dataList.size();
+        this.previewDataList = totalCount > 100? dataList.subList(0, 100) : dataList;
+        if(!preview) {
+            saveData(this.headMap, this.dataList, requestParams);
+        }
     }
 
     /**
@@ -69,6 +134,15 @@ public abstract class DynamicHeadExcelListener extends AnalysisEventListener<Map
      */
     public Map<Integer, String> getHeadMap(){
         return this.headMap;
+    }
+
+    /**
+     * 获取Excel表头
+     *
+     * @return 表头映射
+     */
+    public List<ExcelHelper.TableHead> getTableHead() {
+        return ExcelHelper.buildTableHead(headNameMap, fieldNameMap);
     }
 
     /**
