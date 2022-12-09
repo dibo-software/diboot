f74ac5f0d0b95c6c6147301ad4eb395fd261f1a1
wind <1103642893@qq.com>
2022-12-09 16:25:16 +0800

    * 扩展通用验属表单校性值构造函数

--- diboot-admin-ui/src/utils/validate-form.ts
+++ diboot-admin-ui/src/utils/validate-form.ts
@@ -4,11 +4,18 @@
  * @param validateApi 校验接口
  * @param prop 属性
  * @param id 数据ID
+ * @param param 额外的参数
  */
-export const checkValue = (validateApi: string, prop: string, id?: () => string | undefined) => {
+export const checkValue = (
+  validateApi: string,
+  prop: string,
+  id: () => string | undefined,
+  param?: () => Record<string, unknown>
+) => {
   return (rule: unknown, value: unknown, callback: (error?: string | Error) => void) => {
     if (value) {
-      const params: Record<string, unknown> = { id: id ? id() : undefined }
+      const params: Record<string, unknown> = (param && param()) ?? {}
+      params.id = id()
       params[prop] = value
       api
         .get(validateApi, params)
