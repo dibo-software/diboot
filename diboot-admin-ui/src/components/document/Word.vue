a70f0dbf17eeee69042785774c761cc0fbd6d788
guoshuo <17602463474@163.com>
2022-12-09 16:18:36 +0800

    word/pdf打印传参，word预览参数传空时清空预览内容

--- diboot-admin-ui/src/components/document/Word.vue
+++ diboot-admin-ui/src/components/document/Word.vue
@@ -4,42 +4,62 @@ import { print } from '@/utils/print'
 
 const props = defineProps<{
   // string: 文件链接 ;  Blob | ArrayBuffer: 文件流
-  value: string | Blob | ArrayBuffer
+  value?: string | Blob | ArrayBuffer
 }>()
 
 const context = ref<Blob | ArrayBuffer>()
 const fileName = ref<string>()
 const container = ref()
+const contextDom = ref()
 
 /**
  * 使用docx-preview预览docx文件
  * @param file
  */
-const wordPreview = (file: Blob | ArrayBuffer) => {
-  renderAsync(file, container.value)
+const wordPreview = async (file: Blob | ArrayBuffer) =>
+  await renderAsync(file, container.value).then(
+    () =>
+      new Promise<HTMLElement>(resolve =>
+        // 延时，等待图片渲染
+        setTimeout(() => resolve((contextDom.value = container.value?.children[3]?.children[0])), 100)
+      )
+  )
+
+const wordInit = (value: string | Blob | ArrayBuffer) => {
+  if (typeof value === 'string') {
+    return new Promise<HTMLElement>((resolve, reject) => {
+      api
+        .download(value)
+        .then(res => {
+          fileName.value = res.filename
+          resolve(wordPreview((context.value = res.data)))
+        })
+        .catch(err => {
+          ElMessage.error(err.msg || err.message || '获取文件失败')
+          reject()
+        })
+    })
+  } else return wordPreview((context.value = value))
 }
 
 watch(
   () => props.value,
   value => {
-    if (!value) return
-    if (typeof value === 'string') {
-      api
-        .download(value)
-        .then(res => {
-          fileName.value = res.filename
-          wordPreview((context.value = res.data))
-        })
-        .catch(err => {
-          ElMessage.error(err.msg || err.message || '获取文件失败')
-        })
-    } else wordPreview((context.value = value))
+    if (value) wordInit(value)
+    else container.value && container.value.removeChild(container.value.children[3])
   },
   { immediate: true }
 )
 
+const dispaly = ref(true)
+
 defineExpose({
-  print: () => context.value && print(container.value?.children[3]?.children[0]),
+  print: (value?: string | Blob | ArrayBuffer) => {
+    if (value) {
+      dispaly.value = false
+      wordInit(value).then(print)
+    } else contextDom.value && print(contextDom.value)
+  },
   download: (filename?: string) => {
     if (!context.value) return ElMessage.error('文件内容为空')
     const elink = document.createElement('a')
@@ -58,7 +78,7 @@ defineExpose({
 </script>
 
 <template>
-  <el-scrollbar style="height: 100%">
+  <el-scrollbar v-show="dispaly">
     <div ref="container" />
   </el-scrollbar>
 </template>
