62505fd4e1e272a5fe80c5344e38f7687fd83a58
wuy <1311695042@qq.com>
2023-02-22 11:10:08 +0800

    * 优化前端i18n tree-shaking警告

--- diboot-admin-ui/vite.config.ts
+++ diboot-admin-ui/vite.config.ts
@@ -110,7 +110,8 @@ export default defineConfig(({ command }) => {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
-        '#': fileURLToPath(new URL('./types', import.meta.url))
+        '#': fileURLToPath(new URL('./types', import.meta.url)),
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
       }
     }
     // server: {
