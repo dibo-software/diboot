e94453eb54632b37970bdd40d93769025eb25c54
wind <1103642893@qq.com>
2023-03-22 16:36:34 +0800

    * 添加长耗时接口提示

---
+++ diboot-core-starter/src/main/java/com/diboot/core/filter/WebApiFilter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015-2020, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.diboot.core.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 接口过滤，长耗时接口提示
+ *
+ * @author wind
+ * @version v3.0.0
+ * @date 2023/03/22
+ */
+@Slf4j
+@Component
+public class WebApiFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        long startTime = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        if (elapsedTime > 5000) {
+            log.debug("请求接口；{}，耗时：{}ms，请及时检查优化。", ((HttpServletRequest) request).getRequestURI(), elapsedTime);
+        }
+    }
+
+}
