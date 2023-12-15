21921cbd1623b80362aa2334397df0f3c16a229b
mazc <mazc@dibo.ltd>
2023-12-15 09:56:02 +0800

    * 添加多租户组件

---
+++ diboot-tenant-starter/src/main/java/com/diboot/tenant/starter/TenantAutoConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015-2029, www.dibo.ltd (service@dibo.ltd).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.diboot.tenant.starter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 多租户组件自动初始化
+ *
+ * @author mazc@dibo.ltd
+ * @version v3.2.0
+ * @date 2023/12/15
+ */
+@Slf4j
+@Configuration
+@ComponentScan(basePackages = {"com.diboot.tenant"})
+@MapperScan(basePackages = {"com.diboot.tenant.mapper"})
+public class TenantAutoConfig {
+
+}
