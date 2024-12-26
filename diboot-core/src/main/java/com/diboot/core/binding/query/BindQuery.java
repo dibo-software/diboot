42323d02e2cc70cd044ca472656d4a3dddc5006e
mazc <mazc@dibo.ltd>
2024-12-26 17:15:18 +0800

    * 优化BindQuery的select字段优化，支持剔除大字段避免Oracle Distinct查询报错等

--- diboot-core/src/main/java/com/diboot/core/binding/query/BindQuery.java
+++ diboot-core/src/main/java/com/diboot/core/binding/query/BindQuery.java
@@ -59,9 +59,15 @@ public @interface BindQuery {
     String condition() default "";
 
     /**
-     * 忽略该字段
+     * 构建查询条件时忽略该字段
      */
     boolean ignore() default false;
+
+    /**
+     * 忽略select查询该字段，用于构建查询时剔除Entity中的大字段，避免Oracle等构建DISTINCT查询时报错
+     * @return
+     */
+    boolean ignoreSelect() default false;
 
     /**
      * 查询处理策略：默认忽略空字符串
