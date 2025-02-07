640a6ebd625700143d150b7f497ba62f0d0d3961
mazc <mazc@dibo.ltd>
2025-02-07 10:48:59 +0800

    * 优化example示例代码

--- rest-api-example/src/main/java/com/example/api/config/SpringMvcConfig.java
+++ rest-api-example/src/main/java/com/example/api/config/SpringMvcConfig.java
@@ -1,11 +1,6 @@
 package com.example.api.config;
 
-import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
-import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
-import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -25,20 +20,5 @@ import java.util.List;
 @ComponentScan(basePackages = {"com.example"})
 @MapperScan(basePackages = {"com.example.api.mapper"})
 public class SpringMvcConfig {
-
-    @Autowired
-    private DynamicDataSourceProperties properties;
-
-    @Bean
-    public DataSource dataSource(List<DynamicDataSourceProvider> providers) {
-        DynamicRoutingDataSource dataSource = new DynamicRoutingDataSource(providers);
-        dataSource.setPrimary(this.properties.getPrimary());
-        dataSource.setStrict(this.properties.getStrict());
-        dataSource.setStrategy(this.properties.getStrategy());
-        dataSource.setP6spy(this.properties.getP6spy());
-        dataSource.setSeata(this.properties.getSeata());
-        dataSource.setGraceDestroy(this.properties.getGraceDestroy());
-        return dataSource;
-    }
 
 }
