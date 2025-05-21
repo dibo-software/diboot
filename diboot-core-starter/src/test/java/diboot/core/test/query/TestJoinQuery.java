bd798878395fcc514788d62bf77f07f389ae9d46
mazc <mazc@dibo.ltd>
2025-05-21 17:17:53 +0800

    * update comments

--- diboot-core-starter/src/test/java/diboot/core/test/query/TestJoinQuery.java
+++ diboot-core-starter/src/test/java/diboot/core/test/query/TestJoinQuery.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020, www.dibo.ltd (service@dibo.ltd).
+ * Copyright (c) 2015-2099, www.dibo.ltd (service@dibo.ltd).
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -66,9 +66,6 @@ public class TestJoinQuery {
 
     @Autowired
     UserService userService;
-
-    @Autowired
-    StrIdTestService strIdTestService;
 
     @Autowired
     ProblemService problemService;
