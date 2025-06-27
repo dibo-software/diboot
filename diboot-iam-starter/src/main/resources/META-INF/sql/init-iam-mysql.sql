def4845ccb6d154bd81027ee340c2c297365ade4
mazc <mazc@dibo.ltd>
2025-06-27 13:51:05 +0800

    * 添加dbt_client开放接口初始化sql

--- diboot-iam-starter/src/main/resources/META-INF/sql/init-iam-mysql.sql
+++ diboot-iam-starter/src/main/resources/META-INF/sql/init-iam-mysql.sql
@@ -228,6 +228,19 @@ CREATE TABLE `dbt_system_config`
 ) COMMENT = '系统配置';
 create index idx_dbt_system_config on dbt_system_config (`category`, `prop_key`);
 
+CREATE TABLE dbt_client(
+    `id`        varchar(32) NOT NULL COMMENT 'ID' primary key,
+    `tenant_id` varchar(32) NOT NULL DEFAULT '0' COMMENT '租户ID',
+    `name`      varchar(50) NOT NULL COMMENT '名称',
+    `app_key`   varchar(100) NOT NULL COMMENT 'AppKey',
+    `app_secret` varchar(100) NOT NULL COMMENT 'AppSecret',
+    `status`     varchar(10) DEFAULT 'A' COMMENT '状态',
+    `permissions` varchar(2000) COMMENT '权限',
+    `is_deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '删除标记',
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+) COMMENT = '开放接口';
+
 -- 用户组表
 CREATE TABLE `dbt_iam_group`
 (
