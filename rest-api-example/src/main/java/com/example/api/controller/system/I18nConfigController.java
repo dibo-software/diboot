e1d620b701402bb58e1c70da6c4455d019e0eca9
mazc <mazc@dibo.ltd>
2025-11-07 11:16:16 +0800

    * 优化国际化接口未配置的检查提示

--- rest-api-example/src/main/java/com/example/api/controller/system/I18nConfigController.java
+++ rest-api-example/src/main/java/com/example/api/controller/system/I18nConfigController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.diboot.core.controller.BaseCrudRestController;
 import com.diboot.core.entity.I18nConfig;
 import com.diboot.core.entity.ValidList;
+import com.diboot.core.exception.InvalidUsageException;
 import com.diboot.core.service.I18nConfigService;
 import com.diboot.core.util.V;
 import com.diboot.core.vo.I18nConfigVO;
@@ -27,7 +28,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- * 文件记录 相关Controller
+ * 国际化 相关Controller
  *
  * @author MyName
  * @version 1.0
@@ -38,11 +39,17 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/i18n-config")
 @BindPermission(name = "国际化配置")
-@ConditionalOnBean(I18nConfigService.class)
 public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
 
-    @Autowired
+    @Autowired(required = false)
     private I18nConfigService i18nConfigService;
+
+    private I18nConfigService getI18nConfigService() {
+        if (i18nConfigService == null) {
+            throw new InvalidUsageException("未启用国际化，无法获取相关数据！请开启 diboot.i18n=true 后重试！");
+        }
+        return i18nConfigService;
+    }
 
     /**
      * 获取所有国际化配置
@@ -55,7 +62,7 @@ public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
         Locale locale = LocaleContextHolder.getLocale();
         if (V.isEmpty(locale.getCountry())) queryWrapper.likeRight(I18nConfig::getLanguage, locale.getLanguage());
         else queryWrapper.eq(I18nConfig::getLanguage, locale.getLanguage() + "_" + locale.getCountry());
-        List<I18nConfig> configList = i18nConfigService.getEntityList(queryWrapper);
+        List<I18nConfig> configList = getI18nConfigService().getEntityList(queryWrapper);
         return JsonResult.OK(configList.stream().collect(Collectors.toMap(I18nConfig::getCode, I18nConfig::getContent)));
     }
 
@@ -71,7 +78,7 @@ public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
     @Log(operation = OperationCons.LABEL_LIST)
     @GetMapping
     public JsonResult<Collection<List<I18nConfigVO>>> getViewObjectListMapping(I18nConfig entity, Pagination pagination) throws Exception {
-        return JsonResult.OK(i18nConfigService.getI18nList(entity, pagination)).bindPagination(pagination);
+        return JsonResult.OK(getI18nConfigService().getI18nList(entity, pagination)).bindPagination(pagination);
     }
 
     /**
@@ -84,7 +91,7 @@ public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
     @Log(operation = OperationCons.LABEL_DETAIL)
     @GetMapping("/{code}")
     public JsonResult<List<I18nConfig>> getViewObjectMapping(@PathVariable String code) throws Exception {
-        return JsonResult.OK(i18nConfigService.getEntityList(Wrappers.<I18nConfig>lambdaQuery().eq(I18nConfig::getCode ,code)));
+        return JsonResult.OK(getI18nConfigService().getEntityList(Wrappers.<I18nConfig>lambdaQuery().eq(I18nConfig::getCode ,code)));
     }
 
     /**
@@ -98,7 +105,7 @@ public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
     @BindPermission(name = OperationCons.LABEL_BATCH_UPDATE, code = OperationCons.CODE_WRITE)
     @PostMapping
     public JsonResult<?> createEntityMapping(@Valid @RequestBody ValidList<I18nConfig> entityList) throws Exception {
-        return new JsonResult<>(i18nConfigService.createOrUpdateEntities(entityList));
+        return new JsonResult<>(getI18nConfigService().createOrUpdateEntities(entityList));
     }
 
     /**
@@ -112,7 +119,7 @@ public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
     @BindPermission(name = OperationCons.LABEL_DELETE, code = OperationCons.CODE_WRITE)
     @PostMapping("/batch-delete")
     public JsonResult<?> deleteEntityMapping(@RequestBody List<String> ids) throws Exception {
-        return new JsonResult<>(i18nConfigService.deleteEntities(ids));
+        return new JsonResult<>(getI18nConfigService().deleteEntities(ids));
     }
 
     /**
@@ -128,7 +135,7 @@ public class I18nConfigController extends BaseCrudRestController<I18nConfig> {
         }
         LambdaQueryWrapper<I18nConfig> wrapper = Wrappers.<I18nConfig>lambdaQuery().eq(I18nConfig::getCode, code);
         wrapper.notIn(V.notEmpty(ids), I18nConfig::getId, ids);
-        if (i18nConfigService.exists(wrapper)) {
+        if (getI18nConfigService().exists(wrapper)) {
             return JsonResult.FAIL_VALIDATION("资源标识[" + code + "]已存在");
         }
         return JsonResult.OK();
