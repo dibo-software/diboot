a832ad2291f6f89b965629eecc9720a36e51ca75
mazc <mazc@dibo.ltd>
2026-06-04 15:33:00 +0800

    * 工号为空不识别为重复

--- diboot-iam-starter/src/main/java/com/diboot/iam/service/impl/IamUserServiceImpl.java
+++ diboot-iam-starter/src/main/java/com/diboot/iam/service/impl/IamUserServiceImpl.java
@@ -187,7 +187,7 @@ public class IamUserServiceImpl extends BaseServiceImpl<IamUserMapper, IamUser> 
     @Override
     public boolean isUserNumExists(String id, String userNum) {
         if(V.isEmpty(userNum)){
-            return true;
+            return false;
         }
         LambdaQueryWrapper<IamUser> wrapper = Wrappers.<IamUser>lambdaQuery()
                 .select(IamUser::getUserNum)
